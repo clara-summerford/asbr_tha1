d46dcad90ebe088e57f2ed30a23018da142d36db
Nathan Lovell <nathan.lovell@my.utexas.edu>
2026-02-02 10:32:50 -0600

    Just added code for roll-pitch-yaw Euler angles - work in progress. Also just testing my git commit skills. :)

--- rotMatrixTransform.m
+++ rotMatrixTransform.m
@@ -25,6 +25,26 @@ function rotMatrixTransform(rot)
     alpha = atan2(rot(2,3)/s_beta, rot(1,3)/s_beta);
     gamma = atan2(rot(3,2)/s_beta, -rot(3,1)/s_beta);
     
+    fprintf("ZYZ Euler Angles: \n")
+    fprintf("Alpha: %f\n", alpha)
+    fprintf("Beta: %f\n", beta)
+    fprintf("Gamma: %f\n", gamma)
+
+    % IN PROGRESS: rotation matrix to ZYX Euler angles (RPY)
+
+    % IF beta is in (-pi/2, pi/2) range (not sure how to code this)
+    beta = atan2(-rot(3,1), sqrt(rot(3,2)^2+rot(3,3)^2));
+    %s_beta = sin(beta);
+    alpha = atan2(rot(2,1)/s_beta, rot(1,1)/s_beta);
+    gamma = atan2(rot(3,2)/s_beta, rot(3,3)/s_beta);
+
+    % IF beta is in (pi/2, 3*pi/2) range
+    beta = atan2(-rot(3,1), -sqrt(rot(3,2)^2+rot(3,3)^2));
+    %s_beta = sin(beta);
+    alpha = atan2(-rot(2,3)/s_beta, -rot(1,3)/s_beta);
+    gamma = atan2(-rot(3,2)/s_beta, -rot(3,3)/s_beta);
+    
+    fprintf("ZYX Euler Angles: \n")
     fprintf("Alpha: %f\n", alpha)
     fprintf("Beta: %f\n", beta)
     fprintf("Gamma: %f\n", gamma)
