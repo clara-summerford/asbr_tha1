95085126f4d6db534ab9a9cb7283eca7316861bf
clarasummerford <clara.summerford@ufl.edu>
2026-02-18 10:52:12 -0600

    fixing rotation to axis-angle function and adding skew symmetric function

--- rotToAxisAngle.m
+++ rotToAxisAngle.m
@@ -3,9 +3,23 @@
 function [w, theta] = rotToAxisAngle(rot)
 
     tr = trace(rot);
-    theta = acos((tr-1)/2);
-    axisCalc = [rot(3,2)-rot(2,3), rot(1,3)-rot(3,1), rot(2,1)-rot(1,2)];
-    s_theta = sin(theta);
-    w = (1/(2*s_theta))*axisCalc';
+    
+    % checking first if the rotation matrix is an identity matrix 
+    if rot == eye(3)
+        theta = 0;
+        fprintf("Error: Identity rotation matrix, w is undefined!")
+
+    elseif tr == -1
+        theta = pi;
+        % first option from lectures 
+        w = (1/sqrt(2*(1+rot(3,3))))*[rot(1,3); rot(2,3); 1 + rot(3,3)]; 
+    
+    else 
+        theta = acos((tr-1)/2);
+        w_skew = (1/(2*sin(theta)))*(rot-rot');
+
+        % transforming skew-symmetric representation back to vector 
+        w = [w_skew(3,2); w_skew(1,3); w_skew(2,1)];
+    end
     
 end
