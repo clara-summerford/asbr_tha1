24c5ca35c3c2754d3d6963c4e6b370d3e57d717f
Nathan Lovell <nathanplovell@gmail.com>
2026-04-01 16:01:04 -0500

    Added the tolerance to ensure its robust to little deviations in the matrix element values

--- isValidRot.m
+++ isValidRot.m
@@ -4,25 +4,30 @@
 % This function checks to see if the user-defined rotation matrix (rot) is
 % a valid transformation matrix, i.e., is orthogonal with determinant = +1
 
-function check = isValidRot(rot)
+function test = isValidRot(rot)
 
-dim = size(rot);
+    dim = size(rot);
+    
+    if ~isequal(dim(1),dim(2)) % Checks squareness
+        check = false;
+        return
+    end
+    
+    d = det(rot);
+    orth = rot*rot';
+    
+    % Check for validity
+    tol = 1e-10; % tolerance added to account for difference between 0 and 0.000
+    assert(all(abs(d - 1) < tol, 'all'), "Determinant is not equal to +1!");
+    assert(all(abs(orth - eye(3)) < tol, 'all'), "Rotation matrix is not orthogonal!");
 
-if ~isequal(dim(1),dim(2)) % Checks squareness
-    check = false;
-    return
-end
-
-d = det(rot);
-orth = rot*rot';
-
-if ~isequal(d,1) % Checks determinant is +1
-    check = false;
-elseif ~isequal(orth,eye(3)) % Checks orthogonality
-    check = false;
-else
-    check = true;
-end
+    
+    if ((abs(d - 1)) < tol) && (all(abs(orth - eye(3)) < tol,'all'))
+        test = true;
+        disp('Test PASSED')
+    else
+        test = false;
+    end
 
 end
 
